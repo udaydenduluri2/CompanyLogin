aeb3025acbf0c7dfa33d2a59348cc0f7acfdd48e
Kanaka Durga <dkanakadurga@gmail.com>
2018-06-09 11:15:18 +0100

    Adding New files to Repo for first time - KD

---
+++ authenticate.js
@@ -0,0 +1,53 @@
+
+import * as {$} from 'jquery';
+
+function display() {
+    var obj = {};
+    obj.userName = $("#userName").val();
+    obj.password = $("#password").val();
+   var obj = JSON.stringify(obj);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:49980/api/Authentication/ValidateUser",
+        data: obj,
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8", 
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+        },
+        error: function () {
+            alert("error");
+        }
+    });
+    
+
+    
+//$.post( "http://localhost:49980/api/Authentication/ValidateUser", { "userName": "this.userName", "password": "this.password"} );
+     console.log(obj);
+
+
+}
+
+
+$( document ).ready(function() {
+  // Handler for .ready() called.
+    $("#btnLoginSubmit").click(display);
+    $("#searchIcon").click()
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
